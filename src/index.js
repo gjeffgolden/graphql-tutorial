ec20f6fc949938655d26637c803b87501a537988
gjeffgolden <gjgolden08@gmail.com>
2021-05-15 15:31:11 -0600

    Full CRUD implemented for Links.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,62 @@
 const { ApolloServer } = require('apollo-server');
 
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
-
 let links = [{
     id: 'link-0',
     url: "www.howtographql.com",
     description: "Fullstack tutorial for GraphQL"
+}, {
+    id: 'link-1',
+    url: "wwww.google.com",
+    description: "Search engine."
 }]
+
+//rudimentary way of generating dynamic ID
+let idCount = links.length
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
+        link: (root, { id }) => {
+            const link = links.find(link => link.id === id)
+            return link
+        },
     },
-    Link: {
-        id: (parent) => parent.id,
-        url: (parent) => parent.url,
-        description: (parent) => parent.description
+    Mutation: {
+        post: (parent, args) => {
+            //create link object
+            //args come from schema
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        },
+        updateLink: (root, args) => {
+            const link = links.find(link => link.id === args.id)
+            link = {...link, ...args }
+            return link;
+        },
+        deleteLink: (parent, { id }) => {
+            const removeIndex = links.findIndex(link => link.id === id);
+            const removedLink = links[removeIndex];
+            links.splice(removeIndex, 1);
+            return removedLink;
+        }
     }
 }
 
+
+const fs = require('fs');
+const path = require('path');
+
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf-8'
+    ),
     resolvers
 })
 
