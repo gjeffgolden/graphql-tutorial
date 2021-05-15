3f7a0ef6b34f423e92dfa38b812a91a6450ca4ed
gjeffgolden <gjgolden08@gmail.com>
2021-05-15 14:15:41 -0600

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = `
+    type Query {
+        info: String!
+        feed: [Link!]!
+    }
+`
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+}
+
+let links = [{
+    id: 'link-0',
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+}]
+
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server
+    .listen()
+    .then(({ url }) => 
+        console.log(`Server is running on ${url}`)
+    )
+
